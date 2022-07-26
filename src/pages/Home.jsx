0679626c2dd897c75c5b1d8e63db1e52c4a5eec2
Maksim Aisin <maksim.aisin@mail.ru>
2022-07-26 23:27:46 +0300

    feat: add pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,11 @@ import Categories from '../components/Categories'
 import Sort from '../components/Sort'
 import PizzaBlock from '../components/PizzaBlock'
 import Skeleton from '../components/PizzaBlock/Skeleton'
+import Pagination from '../components/Pagination'
 
 const Home = ({ searchValue }) => {
     const [items, setItems] = React.useState([])
+    const [currentPage, setCurrentPage] = React.useState(1)
     const [isLoading, setIsLoading] = React.useState(true)
     const [categoryId, setCategoryId] = React.useState(0)
     const [sortType, setSortType] = React.useState({
@@ -16,11 +18,12 @@ const Home = ({ searchValue }) => {
     })
 
     const category = categoryId === 0 ? '' : `category=${categoryId}`
+    const search = searchValue ? `&search=${searchValue}` : ''
 
     React.useEffect(() => {
         setIsLoading(true)
         fetch(
-            `https://62d45328cd960e45d456a05c.mockapi.io/items?${category}&sortBy=${sortType.sortOption}&order=${sortType.sortOrder}`
+            `https://62d45328cd960e45d456a05c.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortType.sortOption}&order=${sortType.sortOrder}${search}`
         )
             .then((response) => response.json())
             .then((data) => {
@@ -28,17 +31,15 @@ const Home = ({ searchValue }) => {
                 setIsLoading(false)
             })
         window.scrollTo(0, 0)
-    }, [category, sortType])
+    }, [category, sortType, searchValue, currentPage])
 
     const skeletons = [...new Array(6)].map((_, index) => (
         <Skeleton key={index} />
     ))
 
-    const pizzas = items
-        .filter((item) =>
-            item.title.toLowerCase().includes(searchValue.toLowerCase())
-        )
-        .map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)
+    const pizzas = items.map((pizza) => (
+        <PizzaBlock key={pizza.id} {...pizza} />
+    ))
 
     return (
         <div className='container'>
@@ -53,6 +54,7 @@ const Home = ({ searchValue }) => {
             <div className='content__items'>
                 {isLoading ? skeletons : pizzas}
             </div>
+            <Pagination onChangePage={(number) => setCurrentPage(number)} />
         </div>
     )
 }
